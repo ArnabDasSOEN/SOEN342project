8f648a8ce5452344dda72d432db075ef2bcb1004
Arnab Das <arnabdas1234@hotmail.com>
2024-10-17 20:02:50 -0400

    synching changes

--- app/Lesson Management System/src/View/InstructorConsole.java
+++ app/Lesson Management System/src/View/InstructorConsole.java
@@ -1,5 +1,18 @@
 package View;
+import Controller.OfferingController;
+import Model.Offering;
+import Model.Instructor;
 
 public class InstructorConsole {
+
+    private OfferingController OC;
+
+    public Offering[] viewPotentialOfferings(){
+    Instructor instructor = OC.get
+
+    }
+
+
+
 
 }
